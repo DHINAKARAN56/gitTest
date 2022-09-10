ef5cab9b3ff4a1c190caec8f805dbd6e66f6061c
DHINAKARAN56 <ddhinakaran56@gmail.com>
2022-09-10 12:29:33 +0530

    third commit

---
+++ src/test/java/sampleTest1/SampleTest1.java
@@ -0,0 +1,33 @@
+package sampleTest1;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest1 {
+	@Test
+	public void loginTest() throws IOException {
+		WebDriverManager.chromedriver().setup();
+		WebDriver wd = new ChromeDriver();
+		wd.manage().window().maximize();
+		wd.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		
+		wd.get("htttp://www.google.com/");
+		
+		//	TakesScreenShort -I
+		//	webDriver -I
+		
+		File src = ((TakesScreenshot)wd).getScreenshotAs(OutputType.FILE);
+		FileHandler.copy(src, new File("D:\\DHINA\\Selinium244\\git.Test\\Screenshort\\google.png"));
+		
+	} 
+}
